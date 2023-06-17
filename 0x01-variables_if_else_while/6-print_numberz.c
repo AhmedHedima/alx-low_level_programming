96fa88514db17c7a2de0a4bce55982b1e6ba87e3
Ahmed <ae.com>
2023-06-16 17:22:25 -0700

    6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,23 @@
 #include <stdio.h>
+
+/**
+* main - Entry point
+*
+*Description : print 0-9 using putchar
+*		while using int variable
+*
+*Return : 0 (success)
+*/
+
+int main(void)
+{
+	int x = 0;
+
+	while (x <= 9)
+	{
+		putchar(x + '0');
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
