5611ddc4a50478181d6a4935458649eea5b89f57
Ahmed <ae.com>
2023-06-17 07:57:25 -0700

    101 new

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,20 +14,20 @@ int main(void)
 
 	while (x <= 9)
 	{
-		putchar(x);
+		putchar(x + 48);
 		y = 0;
 		while (y <= 9)
 		{
 			if (y != x)
 			{
-			putchar(y);
+			putchar(y + 48);
 			z = 0;
 			}
 			while (z <= 0)
 			{
 				if (y != z && x != z)
 				{
-				putchar(z);
+				putchar(z + 48);
 				}
 				z++;
 			}
