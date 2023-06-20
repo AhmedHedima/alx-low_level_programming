52f25fd765456bf53e8673e2fb3befdb90abb674
Ahmed <ae.com>
2023-06-20 16:37:21 -0700

    3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/**
+* main - Entry point
+*
+*Description: check input of the function
+*
+*Return: 0(success)
+*/
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	return (0);
+}
