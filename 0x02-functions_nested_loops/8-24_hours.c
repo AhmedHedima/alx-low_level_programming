af55f8684109710f7d4eb1cc20ba067660f14ca7
Ahmed <ae.com>
2023-06-20 18:00:30 -0700

    8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* main - Entry point
+*
+*Description: print all alphabet letter except q and e
+*
+*Return: 0 (success)
+*/
+
+void jack_bauer(void);
+{
+	int m, h;
+
+	for (h = 0; h <= 23; h++)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			_putchar((h / 10) + 48);
+			_putchar((h % 10) + 48);
+			_putchar(':');
+			_putchar((m / 10) + 48);
+			_putchar((m % 10) + 48);
+			_putchar('\n');
+
+		}
+	}
+}
