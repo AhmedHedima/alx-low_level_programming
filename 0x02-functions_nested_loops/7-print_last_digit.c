e0f5182e1487ce8f20d8e94d992f957a06c1465d
Ahmed <ae.com>
2023-06-20 17:50:47 -0700

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* main - Entry point
+*
+*Description: print last digit
+*
+*Return: last digit (success)
+*/
+
+int print_last_digit(int n)
+{
+	int last;
+
+	if (n < 0)
+		last = -1 * (n % 10);
+	else
+		last = n % 10;
+	_putchar(last + '0');
+	return (last);
+
+}
