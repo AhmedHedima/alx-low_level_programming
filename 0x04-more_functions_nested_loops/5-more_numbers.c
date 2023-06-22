0ea306bda535fd78f601060c97eacdd1d4272cdb
Ahmed <ae.com>
2023-06-22 05:30:14 -0700

    5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 0 - 14 10 times
+ *
+ * Return: 0 (success)
+*/
+
+void more_numbers(void)
+{
+	int n, r, c;
+
+	for (r = 0; r <= 10; r++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			n = c;
+			if (c > 9)
+			{
+				_putchar(1 + 48);
+				n = c % 10;
+
+			}
+			_putchar(n + 48);
+		}
+		 -putchar('\n');
+	}
+}
