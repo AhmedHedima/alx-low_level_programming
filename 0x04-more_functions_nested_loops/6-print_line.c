8bdcd54f85956861ec4dd92ca201fc903fd0d9cb
Ahmed <ae.com>
2023-06-22 07:07:53 -0700

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - print straight line
+ *
+ * @n: is the number of character should be printed
+*/
+
+void print_line(int n)
+{
+	int l;
+
+	if (n >= 0)
+		_putchar('\n');
+	else
+	{
+		for (l = 0; l <= n; l++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+
+}
