aec6cc08b21e762c8515d641135bd0dc8558b0b5
Ahmed <ae.com>
2023-07-07 16:09:58 -0700

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - yujm
+ * @argc: plok
+ * @argv: wsdcv
+ * Return: 0 success
+*/
+
+int main(int argc, char *argv[] _attribute_((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
