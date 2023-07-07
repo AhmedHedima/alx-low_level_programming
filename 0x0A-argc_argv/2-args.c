091d00d781a241fc7107d9eee44f56141eb7662f
Ahmed <ae.com>
2023-07-07 16:16:23 -0700

    2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
-/***/
+/**
+ * main - rtghb
+ * @argc: gkm
+ * @argv: ihg
+ * Return: 0
+*/
 
+int main(int argc, char const *argv[])
+{
+	int x = 0;
 
+	while (argc--)
+	{
+		printf("%s\n", argv[x]);
+		x++;
+	}
+	return (0);
+}
