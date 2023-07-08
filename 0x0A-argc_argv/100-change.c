852225aa634c560d7b0187f7cffbf8f7d5d01b6f
Ahmed <ae.com>
2023-07-07 18:03:04 -0700

    100

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,36 @@
+#include  <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - dfgth
+ * @argc: scvb
+ * @argv: dcv
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc == 2)
+	{
+		int i, lc = 0, m = atoi(argv[1]);
+		int c[] = {25, 10, 5, 2, 1};
+
+		for (i = 0; i < 5; i++)
+		{
+			if (m >= c[i])
+			{
+				lc += m / c[i];
+				m = m % c[i];
+				if (m % c[i] == 0)
+					break;
+			}
+		}
+		printf("%d\n", lc);
+	}
+	else
+	{
+		printf("error\n");
+		return (1);
+	}
+	return (0);
+}
