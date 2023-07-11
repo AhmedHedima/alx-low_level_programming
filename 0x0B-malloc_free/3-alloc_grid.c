d1a86ded57989c3e54e4c3410361e4ba9fe09f91
Ahmed <ae.com>
2023-07-11 15:33:05 -0700

    3 new

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,7 +11,7 @@ int **alloc_grid(int width, int height)
 
 	tab = malloc(sizeof(*tab) * height);
 
-	if (width <= 0 || height <= 0 || tab 0)
+	if (width <= 0 || height <= 0 || tab == 0)
 	{
 		return (NULL);
 	}
