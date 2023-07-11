7ab6a2e62aacc9b8b624d1ff7925b8843106393d
Ahmed <ae.com>
2023-07-11 12:55:12 -0700

    1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strdup - iujfiejc
+ * @str: jkvcbhug
+ * Return: 0
+*/
+
+char *_strdup(char *str)
+{
+	int i = 0, s = 0;
+
+	char *m;
+
+	if (str == NULL)
+		return (NULL);
+	for (s = 0; str[s] != '\0'; s++)
+	;
+		m = malloc(size * sizeof(*str) + 1);
+
+	if (m == 0)
+		return (NULL);
+	else
+	{
+		for (; i < s; i++)
+			m[i] = str[i];
+	}
+	return (m);
+}
