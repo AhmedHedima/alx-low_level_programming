f8473f38d70d2b89c64b21e09576cbdbf22f5560
Ahmed <ae.com>
2023-07-20 10:56:39 -0700

    1 nn

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,21 +1,26 @@
 #include "variadic_functions.h"
+
 /**
- * sum_them all - sums variable arguments
+ * print_numbers - prints numbers with separator
+ * @separator: the string separator
  * @n: the number of arguments
- * @...: the integers to sum
- * Return: the integer sum
+ * @...: the integers to print
+ * Return: void
 */
 
-int sum_them_a11(const unsigned int n, ...)
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	int s = 0, i = n;
+	int i = n;
 	va_list ap;
 
 	if (!n)
-		return (0);
+	{
+		printf("\n");
+		return;
+	}
 	va_start(ap, n);
 	while (i--)
-		s += va_arg(ap, int);
+	printf("%d%s", va_arg(ap, int),
+		i ? (separator ? separator : "") : "\n");
 	va_end(ap);
-	return (s);
 }
